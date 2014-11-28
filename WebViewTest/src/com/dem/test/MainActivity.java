69c809a9e9d64990d29ef5719df9764210587e76
BrianAlan <908047679@qq.com>
2014-11-28 14:36:24 +0800

    add  log

--- WebViewTest/src/com/dem/test/MainActivity.java
+++ WebViewTest/src/com/dem/test/MainActivity.java
@@ -7,7 +7,20 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+/**
+ * 
+ *     
+ * 项目名称：WebViewTest    
+ * 类名称：MainActivity    
+ * 类描述：    
+ * 创建人：Brian    
+ * 创建时间：2014年11月28日 下午2:34:51    
+ * 修改人：Brian    
+ * 修改时间：2014年11月28日 下午2:34:51    
+ * 修改备注：    
+ * @version     
+ *
+ */
 public class MainActivity extends Activity {
 	private WebView mWebView;
 	private WebSettings mWebSettings;
